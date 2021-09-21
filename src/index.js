d44b2476001f6be2ee99ef31e35b043fb48f2476
christina makatura <cnmak@comcast.net>
2021-09-20 21:08:38 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildList(e.target.new_task_description.value)
+    form.reset()
+  })
+  
 });
+
+
+function buildList(todo){
+  let p = document.createElement('p')
+  p.textContent = `${todo} `
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+
+}
+
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
